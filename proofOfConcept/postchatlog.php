656ac3fd4b60160f8edb72c1f8521c5b14db93f6
Mark Glavind <mglavi14@student.aau.dk>
2017-10-16 14:04:46 +0200

    bedre filstruktur, og med user join og leave chat beskeder

--- proofOfConcept/postchatlog.php
+++ proofOfConcept/postchatlog.php
@@ -2,7 +2,8 @@
 session_start();
 	$username = $_POST['username'];
 	$text = $_POST['text'];
-	$fp = fopen("chatlog.html", 'a');
+	$sessionID = $_POST['sessionID'];
+	$fp = fopen("SessionsChatlogs/" . $sessionID . ".html", 'a');
 	fwrite($fp, "<div class='msgln'>(".date("G:i").") <b>".$username."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
 	fclose($fp);
 ?>
