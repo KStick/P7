e2c0168156eb378c3f36e7d6d6791c2f82e804c4
Mark Glavind <mglavi14@student.aau.dk>
2017-10-16 09:49:09 +0200

    ændringer jeg ikke kan huske hvad var

--- proofOfConcept/postchatlog.php
+++ proofOfConcept/postchatlog.php
@@ -2,7 +2,7 @@
 session_start();
 	$username = $_POST['username'];
 	$text = $_POST['text'];
-	$fp = fopen("log.html", 'a');
+	$fp = fopen("chatlog.html", 'a');
 	fwrite($fp, "<div class='msgln'>(".date("G:i").") <b>".$username."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
 	fclose($fp);
 ?>
