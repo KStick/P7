56efd19d57adb72e08b5b41c2a49297bbe0357d2
KStick <super-stick@hotmail.com>
2017-10-20 12:13:57 +0200

    Moved Server.py function into flask cli commands and made a run script for linux

--- PytonREST/RestApi.py
+++ PytonREST/RestApi.py
@@ -1,15 +1,56 @@
 from flask import Flask
 from flask import request
+from flask_cors import CORS, cross_origin
 
 import psycopg2
 
 def connect():
 	try:
-		return psycopg2.connect("dbname='sivash' user='sivash' host ='localhost' password='123'")
+		return psycopg2.connect("dbname='user' user='user' host ='localhost' password='123'")
 	except Exception as e:
 		raise	
 	
 app = Flask(__name__)
+CORS(app)
+
+@app.cli.command()
+def initdb():
+	conn = connect()
+	cur = conn.cursor()
+	try:
+		cur.execute("""CREATE TABLE users(id serial PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL);""")
+	except Exception as e:
+		pass
+	try:
+		cur.execute("""CREATE TABLE questions(id serial PRIMARY KEY, username INTEGER REFERENCES users(id) , subject TEXT NOT NULL  ,question TEXT NOT NULL, date_time TIMESTAMP WITH TIME ZONE NOT NULL default CURRENT_TIMESTAMP); """)
+	except Exception as e:
+		pass
+	conn.commit()
+	cur.close()
+	conn.close()
+
+@app.cli.command()
+def seedData():
+	conn = connect()
+	cur = conn.cursor()
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('henry','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('pedro','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('Kenneth','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('Finn','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('Max','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('Morgan','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('Henrik Vanderheim','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('Mark','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('Reggie','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('Jim Sterling','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('OnePunch','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('Joseph','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('Megaman','123','123@a123.com');""")
+	cur.execute("""INSERT INTO users(username, password,email) VALUES('Trent','123','123@a123.com');""")
+	conn.commit()
+	cur.close()
+	conn.close()
+
 
 @app.route('/')
 def hello_world():
