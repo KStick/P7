420933ff86fd5b882e7e79cf5ca86f5aac6c4025
Mark Glavind <mglavi14@student.aau.dk>
2017-10-23 15:46:14 +0200

    an attempt to implement emojis in the messaging platform! :D

--- proofOfConcept/postchatlog.php
+++ proofOfConcept/postchatlog.php
@@ -4,6 +4,6 @@ session_start();
 	$text = $_POST['text'];
 	$sessionID = $_POST['sessionID'];
 	$fp = fopen("SessionsChatlogs/" . $sessionID . ".html", 'a');
-	fwrite($fp, "<div class='msgln'>(".date("G:i").") <b>".$username."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
+	fwrite($fp, "<div id='msgln'>(".date("G:i").") <b>".$username."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
 	fclose($fp);
 ?>
