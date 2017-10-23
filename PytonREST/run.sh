4b2fe8e1cb934b15ab909b3659db8f751c0eb1cc
KStick <super-stick@hotmail.com>
2017-10-23 09:51:42 +0200

    on windows it start the apache server

--- PytonREST/run.sh
+++ PytonREST/run.sh
@@ -7,5 +7,6 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 elif [[ "$OSTYPE" == "win32" ]]; then
 	set FLASK_APP=RestApi.py
 	flask initdb
-	flask run
+	xterm -e flask run
+	xterm -e C:\Program Files\httpd-2.4.27-x64-vc14\Apache24\bin\httpd.exe
 fi
