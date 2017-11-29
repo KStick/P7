e22bda1f36c1617171f1fff5db90daeadfa75b0b
markglavind <markglavind@gmail.com>
2017-11-29 15:08:51 +0000

    Cleaned up the session.html file and fixed postchatlog on linux

--- proofOfConcept/postchatlog.php
+++ proofOfConcept/postchatlog.php
@@ -1,15 +1,18 @@
-<?
+<?php
 session_start();
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 	$username = $_POST['username'];
 	$text = $_POST['text'];
 	$sessionID = $_POST['sessionID'];
-	$print = $username . $sessionID . $text;
-	echo "test";
 	if(is_numeric($_POST["sessionID"])){
-		$fp = fopen( getcwd() .  "/SessionsChatlogs/" . $sessionID . ".html", 'a');
+		$fp = fopen( getcwd() .  "/SessionsChatlogs/" . $sessionID . ".html", "a");
+		error_log("this is a test");
+		error_log($username . $text . $sessionID);
+		error_log(error_get_last());
 		fwrite($fp, "<div class='msgln'>(".date("G:i").") <b>".$username."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
 		fclose($fp);
+		$_SESSION["test"] = error_get_last();
+		$_SESSION["kenneth"] = "kenneth er en cunt";
 	}
 ?>
