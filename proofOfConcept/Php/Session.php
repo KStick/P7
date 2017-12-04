819b17ce27e706600a41bddf15562d5d529f7b40
markglavind <markglavind@gmail.com>
2017-12-04 13:42:24 +0000

    changes to the chat and other minor fixes

--- proofOfConcept/Php/Session.php
+++ proofOfConcept/Php/Session.php
@@ -1,7 +1,7 @@
 <?php
 if(is_numeric($_POST["id"])){
-	if(!file_exists(getcwd()."/Sessions/".$_POST["id"])){
-		$file = fopen(getcwd()."/Sessions/".$_POST["id"], "w");
+	if(!file_exists(getcwd()."/../Sessions/".$_POST["id"])){
+		$file = fopen(getcwd()."/../Sessions/".$_POST["id"], "w");
 		fclose($file);
 	}
 }
