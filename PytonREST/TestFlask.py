0feeaca990aea1786c3601aaeed81df3f7ed4aab
Finnur Beder <fbeder13@student.aau.dk>
2017-12-11 13:56:21 +0100

    test

--- PytonREST/TestFlask.py
+++ PytonREST/TestFlask.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
 from multiprocessing import Pool, TimeoutError
+import threading
 import time
 import os
 import random
 import requests
 import string
+import Queue
 
 def startTest():
 	username = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(19))
@@ -25,23 +27,18 @@ def startTest():
 
 
 def createUser(username, password, email, role):
-	response = requests.post("http://172.17.245.96:5000/GenerateSalt", data={'username':username})
+	response = requests.post("http://localhost:5000/GenerateSalt", data={'username':username})
 	salt = response.text
-	print salt
-	role = requests.post("http://172.17.245.96:5000/CreateUser", data={'username':username, 'key':password, 'email':email, 'role':role, 'salt':salt})
-	print role.text
+	role = requests.post("http://localhost:5000/CreateUser", data={'username':username, 'key':password, 'email':email, 'role':role, 'salt':salt})
 
 def login(username, password):
-	role = requests.post("http://172.17.245.96:5000/validateLogin", data={'username':username, 'HashedPassword':password})
-	print role.text
+	role = requests.post("http://localhost:5000/validateLogin", data={'username':username, 'HashedPassword':password})
 
 def createQuestion(username, subject, question, access):
-	id = requests.post("http://172.17.245.96:5000/Questions", data={'submit':'yes', 'username':username, 'subject':subject, 'question':question, 'access':access})
-	print id.text
+	id = requests.post("http://localhost:5000/Questions", data={'submit':'yes', 'username':username, 'subject':subject, 'question':question, 'access':access})
 
 def getPrivateQuestions():
-	res = requests.get('http://172.17.245.96:5000/Questions', data ={'submit':'no', 'access':'private'})
-	print res.text
+	res = requests.get('http://localhost:5000/Questions', data ={'submit':'no', 'access':'private'})
 	
 def spawnProcesses(num):
     pool = Pool(processes=num)              # start 4 worker processes
@@ -49,8 +46,44 @@ def spawnProcesses(num):
     multiple_results = [pool.apply_async(startTest, ()) for i in range(num)]
     [res.get(timeout=360) for res in multiple_results]
 
-if __name__ == '__main__':
-	spawnProcesses(100)
-	
 
-	#spawnProcesses(50)
+
+exitFlag = True
+
+class myThread (threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.time = []
+	def run(self):
+		while exitFlag:
+			start = time.time()
+			startTest()
+			self.time.append(time.time() - start)
+
+if __name__ == '__main__':
+	threads = []
+	threadID = 1
+
+	start = time.time()
+
+	# Create new threads
+	for i in range(0,20):
+		thread = myThread()
+		thread.start()
+		threads.append(thread)
+	while True:
+		if time.time() - start >= 3 :
+			exitFlag = False
+			print "##################################################"
+			print "Kenneth exit"
+			print "##################################################"
+			break
+
+	# Wait for all threads to complete
+	count = 0
+	for t in threads:
+		count+=1
+		print len(t.time)
+		t.join()
+	print "Exiting Main Thread"
+	print count
